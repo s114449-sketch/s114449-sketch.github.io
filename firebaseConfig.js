f2fce239decccf735a180b051616210f71d9979f
s114449-sketch <s114449@ms2.kghs.kh.edu.tw>
2026-06-08 09:07:18 +0800

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,47 @@
+import { initializeApp }
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+    getAuth
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+    getFirestore
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+
+    apiKey:
+    "YOUR_API_KEY",
+
+    authDomain:
+    "YOUR_PROJECT.firebaseapp.com",
+
+    projectId:
+    "YOUR_PROJECT_ID",
+
+    storageBucket:
+    "YOUR_PROJECT.appspot.com",
+
+    messagingSenderId:
+    "XXXXXXXX",
+
+    appId:
+    "XXXXXXXX"
+};
+
+export const app =
+initializeApp(
+    firebaseConfig
+);
+
+export const auth =
+getAuth(app);
+
+export const db =
+getFirestore(app);
